94cc1bb55e7b51ec575a98324269256d866d30e2
Gabryel Paiva <GabryelPaiva7@gmail.com>
2025-10-30 15:41:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+    function scrollParaSecao(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        
+        if (targetElement) {
+            const targetPosition = targetElement.offsetTop - 80;
+            const startPosition = window.pageYOffset;
+            const distance = targetPosition - startPosition;
+            const duration = 1000; 
+            let startTime = null;
+            function animation(currentTime) {
+                if (startTime === null) startTime = currentTime;
+                const timeElapsed = currentTime - startTime;
+                const run = easeInOutQuad(timeElapsed, startPosition, distance, duration);
+                window.scrollTo(0, run);
+                if (timeElapsed < duration) {
+                    requestAnimationFrame(animation);
+                }
+            }
+            function easeInOutQuad(t, b, c, d) {
+                t /= d / 2;
+                if (t < 1) return c / 2 * t * t + b;
+                t--;
+                return -c / 2 * (t * (t - 2) - 1) + b;
+            }
+            requestAnimationFrame(animation);
+        }
+    }
+
+
+    document.querySelectorAll('.menu-link').forEach(link => {
+        link.addEventListener('click', scrollParaSecao);
+    });
+
+    function enviarWhats(event) {
+        event.preventDefault();
+        const nome= document.getElementById('nome').value;
+        const mensagem= document.getElementById('mensagem');
+        const telefone= '5579999678249'; 
+        const texto = `Olá, meu nome é ${nome}. ${mensagem}`;   
+        const msgFormatada = encodeURIComponent(texto);
+        const url = `https://wa.me/${telefone}?text=${msgFormatada}`;
+        window.open(url, '_blank');
+        consele.log(url);
+    }
+    const nome= document.getElementById('nome');
+    const mensagem= document.getElementById('mensagem');
+    const telefone= '5579999678249'; 
+
+    const texto = `Olá, meu nome é ${nome}. ${mensagem}`;   
+    const msgFormatada = encodeURIComponent(texto);
+
+    const url = `https://whatsa.me/${telefone}/?t=${msgFormatada}`;
+
+    window.open(url, '_blank');
+
+        window.addEventListener('scroll', function() {
+            const nav = document.querySelector('.navegacao');
+            if (window.scrollY > 100) {
+                nav.classList.add('nav-scrolled');
+            } else {
+                nav.classList.remove('nav-scrolled');
+            }
+        });
+    
